1bd36571dd5926497d14785dcb82153e134ab496
Mithun H R <mithunhr@Mithuns-MacBook-Pro.local>
2022-04-18 16:45:40 +0530

    cleaning up keeping only files required for automation

--- edb12_backup/dn/rslt-formated.sh
+++
@@ -1,3 +0,0 @@
-echo "Client	TPS"
-echo "------	---"
-paste <(grep threads: $1/*log|awk '{print $4}') <(grep includ $1/*log|awk '{print $3}')
--- edb12_backup/dn/rslt.sh
+++
@@ -1,4 +0,0 @@
-echo raw result
-
-grep threads: $1/*log|awk '{print $4}'
-grep includ $1/*log|awk '{print $3}'
